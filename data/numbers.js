cfb8538c677738a312d3e68e8904ff5d118fd705
lova2246 <www.zack01jahada@gmail.com>
2026-09-24 11:47:23 +0530

    Add files via upload

--- data/numbers.js
+++ data/numbers.js
@@ -1,4 +1,1 @@
-window.OL_CHIKI_NUMBERS = [
-  {id:0,g:"᱐",value:0},{id:1,g:"᱑",value:1},{id:2,g:"᱒",value:2},{id:3,g:"᱓",value:3},{id:4,g:"᱔",value:4},
-  {id:5,g:"᱕",value:5},{id:6,g:"᱖",value:6},{id:7,g:"᱗",value:7},{id:8,g:"᱘",value:8},{id:9,g:"᱙",value:9}
-];
+window.OL_CHIKI_NUMBERS=[{"g": "᱐", "value": "0"}, {"g": "᱑", "value": "1"}, {"g": "᱒", "value": "2"}, {"g": "᱓", "value": "3"}, {"g": "᱔", "value": "4"}, {"g": "᱕", "value": "5"}, {"g": "᱖", "value": "6"}, {"g": "᱗", "value": "7"}, {"g": "᱘", "value": "8"}, {"g": "᱙", "value": "9"}];
